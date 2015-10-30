636d46d22a9c662e77301d76eb221804ca478679
Nawal Nabila <nawalnabila20@gmail.com>
2015-10-30 16:45:05 +0700

    Vector3D Java

---
+++ 01-Vector/Vector3D.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Vector3D here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Vector3D
+{
+   double x;
+   double y;
+   double z;
+   
+   public Vector3D(double x, double y, double z)
+   {
+       this.x = x;
+       this.y = y;
+       this.z = z;
+       
+    }
+}
