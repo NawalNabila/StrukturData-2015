3c167102d63e49813ba0f8ccf53a8c58e0e16b66
Nawal Nabila <nawalnabila20@gmail.com>
2015-10-30 17:20:05 +0700

    no message

--- 01-Vector/Vector3D.java
+++ 01-Vector/Vector3D.java
@@ -7,15 +7,24 @@
  */
 public class Vector3D
 {
-   double x;
-   double y;
-   double z;
+   private double x;
+   private double y;
+   private double z;
    
    public Vector3D(double x, double y, double z)
    {
        this.x = x;
        this.y = y;
        this.z = z;
-       
     }
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public double getZ(){
+        return z;
+    }
+    
 }
